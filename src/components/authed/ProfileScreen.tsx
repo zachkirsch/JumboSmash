7c575d227db4a10e26fb480fea9c3dceec49e897
Winnona DeSombre <winnona.desombre@tufts.edu>
2018-01-03 00:05:07 +0800

    reporting, logout, and text changing bio/name all work on front end. Code is messy though.

--- src/components/authed/ProfileScreen.tsx
+++ src/components/authed/ProfileScreen.tsx
@@ -1,37 +1,37 @@
 import React, { PureComponent } from 'react'
-import { connect, Dispatch } from 'react-redux'
-import { Modal, View, Text, TextInput, Button, StyleSheet /*Image, CameraRoll*/} from 'react-native'
+import { Modal, View, Text, TextInput, Button, StyleSheet, Alert /*Image, CameraRoll*/} from 'react-native'
 import { NavigationTabScreenOptions } from 'react-navigation'
 import { Icon } from 'react-native-elements'
 import Ionicons from 'react-native-vector-icons/Ionicons'
-import { logout } from '../../services/auth'
-import { RootState } from '../../redux'
-/*import ImagePicker from 'react-native-image-picker'*/
+import {default as SettingsMenuScreen} from './SettingsMenu'
 
-//TODO: CAMERA ROLL, BACKEND PERSISTENCE, Name editing (?), ADDITIONAL FEATURES (TAGS, etc?)
 
-interface DispatchProps {
-  onLogout: () => void
-}
-
-type Props = DispatchProps
+//TODO: , CAMERA ROLL, BACKEND PERSISTENCE, ADDITIONAL FEATURES (TAGS, etc?)
+//Bugs: 1) this isn't actually sanitizing input before changing it in state...? Fix with backend persistence?
+/*       2) Image part of react-native not working, static links or uri's*/
 interface State {
   bio: string,
   avatarSource: string,
   name: string,
   age: number,
-  modalVisible: boolean
+  modalVisible: boolean,
+  nameEditing: boolean,
+  bioEditing: boolean,
 }
 
-class ProfileScreen extends PureComponent<Props, State> {
+const ALPHA_REGEX = /^[A-Za-z -']+$/
 
-  constructor(props: Props) {
+class ProfileScreen extends PureComponent<{}, State> {
+
+  constructor(props: {}) {
      super(props);
-     this.state = { bio: 'Insert witty bio here.',
+     this.state = { bio: "I haven't set up a bio yet." +'\n' +"I have 240 characters to do so!",
                     avatarSource: 'none',
                     name: 'Dummy Name',
                     age: 21,
-                    modalVisible: false
+                    modalVisible: false,
+                    nameEditing: false,
+                    bioEditing: false
                   };
    }
 
@@ -61,57 +61,54 @@ class ProfileScreen extends PureComponent<Props, State> {
 
 /*********************** HELPER FUNCTIONS FOR RENDERING ***********************/
   private renderNameSection = () => {
-    return <View style={{flex: 1.3, marginTop: 8, flexDirection: 'column', borderBottomColor: '#D3D3D3',  borderBottomWidth: 1}} >
-      {this.renderName()}
-      {this.renderNameEdit()}
-    </View>
+    if (this.state.nameEditing) {
+      return this.renderNameEdit()
+    }
+    else {
+      return this.renderName()
+    }
   }
 
   private renderModal = () => {
-    return <View>
-       <Modal animationType = {"slide"} transparent = {false}
-          visible = {this.state.modalVisible}>
-          <View style={styles.modal}>
-             <Text style = {styles.titleText}>If You Don't See Your Preferred Name 🙁...</Text>
-             </View>
-             <View style={styles.modal2}>
-               <Text style={styles.SignOfftext}> Jumbosmash pulls your default name from the Tufts White Pages, so we understand that this may not be your preferred name.</Text>
-               <Text></Text>
-               <Text style={styles.SignOfftext}> Please email [INSERT JUMBOSMASH EMAIL HERE] with the title "I would like to change my name". </Text>
-               <Text></Text>
-               <Text style={styles.SignOfftext}>Include your current White Pages name and your preferred name, and our team will change it in 24 hours!</Text>
-               {this.renderSignoff()}
-               <View style={styles.modal}/>
-             <Button
-               onPress = {() => {this.toggleModal(false)}}
-               title="Okay, thanks for letting me know!"
-               color="#841584"
-               accessibilityLabel="Okay, thanks for letting me know."
-             />
-             </View>
-       </Modal>
-    </View>
-  }
-  private renderSignoff = () => {
-    return (
-      <View style={styles.signoff}>
-        <Text style={styles.SignOfftext}>xoxo 💕</Text>
-        <Text style={styles.SignOfftext}>The Jumbosmash Team 🐘</Text>
-      </View>
-    )
+    return <Modal animationType = {"slide"} transparent = {false} visible = {this.state.modalVisible}>
+        <SettingsMenuScreen />
+        <Button
+          onPress = {() => {this.toggleModal(false)}}
+          title="Back to Profile"
+          color="#841584"
+          accessibilityLabel="Back to Profile"
+        />
+      </Modal>
   }
 
   private toggleModal(visible: boolean) {
      this.setState({ modalVisible: visible });
   }
 
+  private toggleNameEdit(visible: boolean) {
+       this.setState({ nameEditing: visible });
+  }
+
+  private toggleBioEdit(visible: boolean) {
+       this.setState({ bioEditing: visible });
+  }
+
   private renderBioSection = () => {
-    return <View style={{flex: 4, flexDirection: 'row'}} >
-        <View style={{flex: 4, flexDirection: 'column'}}>
-            <Text style={styles.typeText}></Text>
-            {this.renderBio()}
-        </View>
-    </View>
+    if (this.state.bioEditing){
+      return <View style={{flex: 4, flexDirection: 'row'}} >
+          <View style={{flex: 4, flexDirection: 'column'}}>
+              <Text style={styles.typeText}></Text>
+              {this.renderBioEdit()}
+          </View>
+      </View>
+    } else {
+          return <View style={{flex: 4, flexDirection: 'row'}} >
+              <View style={{flex: 4, flexDirection: 'column'}}>
+                  <Text style={styles.typeText}></Text>
+                  {this.renderBio()}
+              </View>
+          </View>
+    }
   }
   private renderTopBar = () => {
     return <View style={[styles.center]}><Text></Text>
@@ -147,22 +144,44 @@ class ProfileScreen extends PureComponent<Props, State> {
             </View>
     }
 
-    private renderName = () => {
-      return <View style={[styles.textContainer]}>
+    renderNameEdit() {
+      return <View style={{flex: 1.3, marginTop: 8, flexDirection: 'column', borderBottomColor: '#D3D3D3',  borderBottomWidth: 1}} >
+        <View style={[styles.textContainer]}>
+      <TextInput maxLength={20}
+        style={styles.titleText}
+        placeholder={this.state.name}
+        onChangeText={(name) => this.setState({name})}
+      />
             <Text style={styles.titleText}>{this.state.name}, {this.state.age}</Text>
       </View>
+      <View style={[styles.textContainer]}>
+            <View style={{flex: 2, flexDirection: 'row'}}>
+            <Button
+              onPress = {() => {this.onTextSubmit('name')}}
+              title="Confirm"
+              color="#841584"
+              accessibilityLabel="Confirm"
+            />
+            </View>
+        </View>
+        </View>
     }
 
-    private renderNameEdit = () => {
-        return <View style={[styles.textContainer]}>
+    renderName() {
+      return <View style={{flex: 1.3, marginTop: 8, flexDirection: 'column', borderBottomColor: '#D3D3D3',  borderBottomWidth: 1}}>
+      <View style={[styles.textContainer]}>
+            <Text style={styles.titleText}>{this.state.name}, {this.state.age}</Text>
+      </View>
+      <View style={[styles.textContainer]}>
               <View style={{flex: 2, flexDirection: 'row'}}>
               <Button
-                onPress = {() => {this.toggleModal(true)}}
-                title="Edit Name"
+                onPress = {() => {this.toggleNameEdit(true)}}
+                title="Edit My Name"
                 color="#841584"
-                accessibilityLabel="Edit your name!"
+                accessibilityLabel="Edit My Name"
               />
               </View>
+          </View>
           </View>
     }
 
@@ -170,32 +189,70 @@ class ProfileScreen extends PureComponent<Props, State> {
       return <View style={[styles.textContainer]}>
         <Text style={styles.typeText}>Bio</Text>
         <View style={{padding: 10}}>
-            <TextInput
+            <Text style={styles.bioText}>{this.state.bio}</Text>
+        </View>
+          <View style={{flex: 2, flexDirection: 'row', alignSelf: 'flex-start'}}>
+        <Button
+          onPress = {() => {this.toggleBioEdit(true)}}
+          title="Edit Bio"
+          color="#841584"
+          accessibilityLabel="Edit Bio"
+        />
+        </View>
+      </View>
+    }
+
+    private renderBioEdit = () => {
+      return <View style={[styles.textContainer]}>
+        <Text style={styles.typeText}>Bio</Text>
+        <View style={{padding: 10}}>
+            <TextInput maxLength={240}
               style={styles.bioText}
               placeholder={this.state.bio}
               onChangeText={(bio) => this.setState({bio})}
             />
         </View>
+        <Button
+          onPress = {() => {this.onTextSubmit('bio')}}
+          title="Confirm Bio"
+          color="#841584"
+          accessibilityLabel="Confirm Bio"
+        />
       </View>
+    }
+
+    private onTextSubmit(input: string) {
+      if (input == 'name'){
+        if (!ALPHA_REGEX.test(this.state.name)) {
+          Alert.alert(
+            'Invalid Name Format, Try Again',
+          )
+        } else {
+            this.toggleNameEdit(false)
+        }
+      } else if (input == 'bio') {
+         var cleanString = this.state.bio.replace(/[|*{};$%"<>()]/g, "");
+         cleanString = cleanString.replace(/[&]/g, "and");
+         cleanString = cleanString.replace(/[+]/g, "plus");
+         cleanString = cleanString.replace(/[@]/g, "at");
+         cleanString = cleanString.replace(/[=]/g, "equals");
+         this.setState({bio: cleanString});
+         this.toggleBioEdit(false);
+      }
+
     }
 
     private renderLogoutSection = () => {
       return <View style={{flex: 1, flexDirection: 'row'}} >
           <View style={[styles.container, styles.center]}>
-              <Button onPress={this.props.onLogout} title='Logout'/>
+              <Button onPress = {() => {this.toggleModal(true)}} title='Settings Menu'/>
           </View>
       </View>
     }
 /*****************************************************************************/
 }
 
-const mapDispatchToProps = (dispatch: Dispatch<RootState>): DispatchProps => {
-  return {
-    onLogout: () => dispatch(logout()),
-  }
-}
-
-export default connect(undefined, mapDispatchToProps)(ProfileScreen)
+export default ProfileScreen
 
 const styles = StyleSheet.create({
   container: {
@@ -209,6 +266,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   textContainer: {
+    paddingLeft: 5,
     flex: 2,
     flexDirection: 'column',
     backgroundColor: 'white'
@@ -222,6 +280,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   typeText:{
+
     fontSize: 22,
     fontWeight: 'bold',
     fontFamily: 'Helvetica',
@@ -230,25 +289,5 @@ const styles = StyleSheet.create({
   bioText: {
     fontSize: 20,
     fontFamily: 'Helvetica'
-  },
-
-  modal: {
-     flex: 1,
-     alignItems: 'center',
-     backgroundColor: 'white',
-     padding: 20
-  },
-  modal2: {
-     flex: 6,
-     alignItems: 'center',
-     backgroundColor: 'white',
-     padding: 40
-  },
-  SignOfftext: {
-    fontSize: 19,
-    color: '#3f3f3f',
-  },
-  signoff: {
-    marginTop: 20,
   }
 })
