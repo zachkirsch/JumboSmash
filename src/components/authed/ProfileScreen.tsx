5b190fa9138933f2c1d45bf249a784c31331caa7
Winnona DeSombre <winnona.desombre@tufts.edu>
2018-01-02 14:29:04 +0800

    separating code somewhat, trying modal

--- src/components/authed/ProfileScreen.tsx
+++ src/components/authed/ProfileScreen.tsx
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react'
 import { connect, Dispatch } from 'react-redux'
-import { View, Text, TextInput, Button, StyleSheet, /*Image, CameraRoll*/} from 'react-native'
+import { Modal, View, Text, TextInput, Button, StyleSheet, /*Image, CameraRoll*/} from 'react-native'
 import { NavigationTabScreenOptions } from 'react-navigation'
 import { Icon } from 'react-native-elements'
 import Ionicons from 'react-native-vector-icons/Ionicons'
@@ -17,14 +17,22 @@ interface DispatchProps {
 type Props = DispatchProps
 interface State {
   bio: string,
-  avatarSource: string
+  avatarSource: string,
+  name: string,
+  age: number,
+  modalVisible: boolean
 }
 
 class ProfileScreen extends PureComponent<Props, State> {
 
   constructor(props: Props) {
      super(props);
-     this.state = { bio: 'Useless Placeholder', avatarSource: 'none'};
+     this.state = { bio: 'Insert witty bio here.',
+                    avatarSource: 'none',
+                    name: 'Dummy Name',
+                    age: 21,
+                    modalVisible: false
+                  };
    }
 
   static navigationOptions: NavigationTabScreenOptions = {
@@ -39,78 +47,108 @@ class ProfileScreen extends PureComponent<Props, State> {
 
   render() {
     return (
-
       <View style={{flex: 1, marginTop: 8, flexDirection: 'column', backgroundColor: '#EFEFEF'}}>
-            <View style={[styles.center]}>
-                <Text></Text>
-                <Text style={styles.titleText}>Edit Info</Text>
-            </View>
-            <View style={{flex: 4, flexDirection: 'row', borderBottomColor: '#D3D3D3',
-            borderBottomWidth: 3}}>
-              <View style={{flex: 4, margin: 4, justifyContent: 'flex-end', backgroundColor: 'steelblue'}}>
-
-              <View style={{alignSelf: 'flex-end'}}><Icon name='add-circle' size={50} color='#EFEFEF'
-                    onPress={() => {
-                     //TODO: INSERT CAMERA ROLL
-                    }}
-              /></View>
-              </View>
-              <View style={{flex: 2, margin: 4, marginTop: 4, backgroundColor: '#EFEFEF'}} >
-                    <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-end', backgroundColor: 'midnightblue'}}>
-                      <View style={{alignSelf: 'flex-end'}}><Icon name='add-circle' size={30} color='#EFEFEF'
-                            onPress={() => {
-                             //TODO: INSERT CAMERA ROLL
-                            }}
-                      /></View>
-                    </View>
-                    <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-end', backgroundColor: 'steelblue'}}>
-                      <View style={{alignSelf: 'flex-end'}}><Icon name='add-circle' size={30} color='#EFEFEF'
-                            onPress={() => {
-                             //TODO: INSERT CAMERA ROLL
-                            }}
-                      /></View>
-                    </View>
-              </View>
-            </View>
-            <View style={{flex: 1, marginTop: 8, flexDirection: 'column', borderBottomColor: '#D3D3D3',  borderBottomWidth: 1}} >
-              <View style={[styles.textContainer]}>
-                    <Text style={styles.titleText}>Dummy Profile, 22</Text>
-              </View>
-              <View style={[styles.textContainer]}>
-                  <View style={{flex: 1, flexDirection: 'row'}}>
-                  <Button
-                    onPress={this.props.onLogout} //TODO: CHANGE THIS TO CHANGE NAME.
-                    title="Edit Name"
-                    color="#841584"
-                    accessibilityLabel="Edit your name!"
-                  />
-                  </View>
-                <View style={{flex: 1, flexDirection: 'row'}}/>
-              </View>
-            </View>
-            <View style={{flex: 4, flexDirection: 'row'}} >
-                <View style={{flex: 4, flexDirection: 'column'}}>
-                    <Text style={styles.typeText}></Text>
-                    <View style={[styles.textContainer]}>
-                      <Text style={styles.typeText}>Bio</Text>
-                      <View style={{padding: 10}}>
-                          <TextInput
-                            style={styles.bioText}
-                            placeholder="Insert witty bio here"
-                            onChangeText={(bio) => this.setState({bio})}
-                          />
-                      </View>
-                    </View>
-                </View>
-            </View>
-            <View style={{flex: 1, flexDirection: 'row'}} >
-                <View style={[styles.container, styles.center]}>
-                    <Button onPress={this.props.onLogout} title='Logout'/>
-                </View>
-            </View>
+            {this.renderTopBar() //TODO: for some reason the top bar isn't working
+            }
+            {this.renderProfilePictures()}
+            {this.renderNameSection()}
+            {this.renderBioSection()}
+            {this.renderLogoutSection()}
       </View>
     )
   }
+
+/*********************** HELPER FUNCTIONS FOR RENDERING ***********************/
+  private renderNameSection = () => {
+    return <View style={{flex: 1.3, marginTop: 8, flexDirection: 'column', borderBottomColor: '#D3D3D3',  borderBottomWidth: 1}} >
+      {this.renderName()}
+      {this.renderNameEdit()}
+    </View>
+  }
+
+  private renderBioSection = () => {
+    return <View style={{flex: 4, flexDirection: 'row'}} >
+        <View style={{flex: 4, flexDirection: 'column'}}>
+            <Text style={styles.typeText}></Text>
+            {this.renderBio()}
+        </View>
+    </View>
+  }
+  private renderTopBar = () => {
+    return <View style={[styles.center]}><Text></Text>
+        <Text style={styles.titleText}>Edit Info</Text>
+        </View>
+  }
+  private renderProfilePictures = () => {
+        return <View style={{flex: 4, flexDirection: 'row', borderBottomColor: '#D3D3D3',
+            borderBottomWidth: 3}}>
+                    <View style={{flex: 4, margin: 4, justifyContent: 'flex-end', backgroundColor: 'steelblue'}}>
+                        <View style={{alignSelf: 'flex-end'}}><Icon name='add-circle' size={50} color='#EFEFEF'
+                              onPress={() => {
+                               //TODO: INSERT CAMERA ROLL
+                              }}
+                        /></View>
+                    </View>
+                    <View style={{flex: 2, margin: 4, marginTop: 4, backgroundColor: '#EFEFEF'}} >
+                          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-end', backgroundColor: 'midnightblue'}}>
+                            <View style={{alignSelf: 'flex-end'}}><Icon name='add-circle' size={30} color='#EFEFEF'
+                                  onPress={() => {
+                                   //TODO: INSERT CAMERA ROLL
+                                  }}
+                            /></View>
+                          </View>
+                          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-end', backgroundColor: 'steelblue'}}>
+                            <View style={{alignSelf: 'flex-end'}}><Icon name='add-circle' size={30} color='#EFEFEF'
+                                  onPress={() => {
+                                   //TODO: INSERT CAMERA ROLL
+                                  }}
+                            /></View>
+                          </View>
+                    </View>
+            </View>
+    }
+
+    private renderName = () => {
+      return <View style={[styles.textContainer]}>
+            <Text style={styles.titleText}>{this.state.name}, {this.state.age}</Text>
+      </View>
+    }
+
+    private renderNameEdit = () => {
+        return <View style={[styles.textContainer]}>
+              <View style={{flex: 2, flexDirection: 'row'}}>
+              <Button
+                onPress={this.props.onLogout} //TODO: CHANGE THIS TO CHANGE NAME.
+                title="Edit Name"
+                color="#841584"
+
+                accessibilityLabel="Edit your name!"
+              />
+              </View>
+          </View>
+    }
+
+    private renderBio = () => {
+      return <View style={[styles.textContainer]}>
+        <Text style={styles.typeText}>Bio</Text>
+        <View style={{padding: 10}}>
+            <TextInput
+              style={styles.bioText}
+              placeholder={this.state.bio}
+              onChangeText={(bio) => this.setState({bio})}
+            />
+        </View>
+      </View>
+    }
+
+    private renderLogoutSection = () => {
+      return <View style={{flex: 1, flexDirection: 'row'}} >
+          <View style={[styles.container, styles.center]}>
+              <Button onPress={this.props.onLogout} title='Logout'/>
+          </View>
+      </View>
+    }
+/*****************************************************************************/
 }
 
 const mapDispatchToProps = (dispatch: Dispatch<RootState>): DispatchProps => {
@@ -118,7 +156,6 @@ const mapDispatchToProps = (dispatch: Dispatch<RootState>): DispatchProps => {
     onLogout: () => dispatch(logout()),
   }
 }
-
 
 export default connect(undefined, mapDispatchToProps)(ProfileScreen)
 
