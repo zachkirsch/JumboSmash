81b131f41ae48148e9c1712b9826d72b65a8dd3f
Zach Kirsch <zachkirsch@gmail.com>
2018-01-03 02:22:58 -0500

    intergrate redux-persist v4

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,28 @@
-import { applyMiddleware, createStore } from 'redux'
+import {AsyncStorage} from 'react-native'
+import { compose, applyMiddleware, createStore } from 'redux'
 import logger from 'redux-logger'
 import createSagaMiddleware from 'redux-saga'
+import { autoRehydrate, persistStore } from 'redux-persist'
 import { rootSaga } from './rootSaga'
 import { rootReducer } from './rootReducer'
 import { RootState } from './types'
+import { setRehydrated } from '../services/redux'
 
 const sagaMiddleware = createSagaMiddleware()
 
 export const reduxStore = createStore(
   rootReducer,
-  applyMiddleware(sagaMiddleware, logger)
+  undefined,
+  compose(
+    applyMiddleware(sagaMiddleware, logger),
+    autoRehydrate()
+  )
 )
+
+persistStore(reduxStore, {storage: AsyncStorage}, () => {
+  reduxStore.dispatch(setRehydrated())
+})
+
 export const getState = (): RootState => reduxStore.getState() as RootState
 
 sagaMiddleware.run(rootSaga)
