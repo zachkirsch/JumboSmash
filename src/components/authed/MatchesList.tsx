5c6730e8de523b246c639bc57ead48e395f0af0b
yzan424 <yuki.zaninovich@tufts.edu>
2018-02-16 17:07:43 -0500

    Made matches list skeleton

--- src/components/authed/MatchesList.tsx
+++ src/components/authed/MatchesList.tsx
@@ -1,48 +1,72 @@
 import React, { PureComponent } from 'react'
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native'
+import { View, Button, StyleSheet, FlatList } from 'react-native'
 import { NavigationScreenProps } from 'react-navigation'
 
 type Props = NavigationScreenProps<{}>
 
+interface User {
+  key: string
+}
+
+interface x {
+  item: User
+}
+
 interface State {
-  userToChat: string
+  userToChat: User[]
 }
 
 class MatchesList extends PureComponent<Props, State> {
 
-  constructor(props: Props) {
-    super(props)
-    this.state = {
-      userToChat: 'Tony Monaco',
+    constructor(props: Props) {
+        super(props)
+        this.state = {
+            userToChat: [
+                {key: 'Tony Monaco'},
+                {key: 'Yuki Zaninovich'},
+                {key: 'Zach Kirsch'},
+                {key: 'Shanshan Duan'},
+                {key: 'Chris Gregg'},
+            ]
+        }
     }
-  }
 
-  public render() {
-    return (
-      <View style={[styles.container, styles.center]}>
-        <Text>This is the matches screen</Text>
-        <View style={styles.openChatContainer}>
-          <TextInput
-            style={styles.input}
-            placeholder='Chat with...'
-            onChangeText={this.onChangeUserToChat}
-            value={this.state.userToChat}
-          />
-          <Button onPress={this.openChatScreen} title='Chat'/>
-        </View>
-      </View>
-    )
-  }
+    public render() {
+        return (
+            <View style={[styles.container]}>
+                <FlatList
+                    data={this.state.userToChat}
+                    renderItem={this.renderItem}
+                />
+                {/*<TextInput*/}
+                {/*style={styles.input}*/}
+                {/*placeholder='Chat with...'*/}
+                {/*onChangeText={this.onChangeUserToChat}*/}
+                {/*value={this.state.userToChat}*/}
+                {/*/>*/}
+                {/*<Button onPress={this.openChatScreen} title='Chat'/>*/}
+            </View>
+        )
+    }
 
-  private onChangeUserToChat = (userToChat: string) => {
-    this.setState({
-      userToChat,
-    })
-  }
+    // private onChangeUserToChat = (userToChat: string) => {
+    //     this.setState({
+    //         userToChat,
+    //     })
+    // }
 
-  private openChatScreen = () => {
-    this.props.navigation.navigate('Chat', { user: this.state.userToChat })
-  }
+    private openChatScreen(userToChat: string) {
+        this.props.navigation.navigate('Chat', {user: userToChat})
+    }
+
+    private renderItem = (y: x) => {
+        return (
+            <Button
+                onPress={() => this.openChatScreen(y.item.key)}
+                title={y.item.key}
+            />
+        )
+    }
 }
 
 export default MatchesList
