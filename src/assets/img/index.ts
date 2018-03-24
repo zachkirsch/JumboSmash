d9d18ba9397e82345b5cc4cda481e72fbec997eb
Zach Kirsch <zachkirsch@gmail.com>
2018-03-23 20:29:41 -0400

    resize react images to be 100x100

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -1,9 +1,8 @@
 const images: { [imageName: string]: any } = { /* tslint:disable-line:no-any */
-  acornhead: require('./acornhead.jpg'),
+  acornhead: require('./acornhead.png'),
   goBos: require('./goBos.png'),
   tonymonaco: require('./tonymonaco.png'),
   jumbo2018: require('./jumbo2018.png'),
-  square: require('./square.jpg'),
 }
 
 export { images as Images }
