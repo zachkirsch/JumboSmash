51feb394b4c05ffcd35e3411a707ab8fd2a1b718
Winnona DeSombre <winnona@Winnonas-MacBook-Pro-4.local>
2018-04-15 18:02:05 -0400

    merging with develop the tutorial and beginning of reacts

--- src/components/authed/swipe/Card.tsx
+++ src/components/authed/swipe/Card.tsx
@@ -343,32 +343,7 @@ class Card extends PureComponent<Props, State> {
                   onPress: () => {this.setState({reactDialogView: false})} //TODO: send tags
               }} >
               <View>
-              <View style={styles.reactColumns}>
-                <View style={styles.reactColumn}>
-                <View style={styles.reactGroup}>
-                  <TouchableOpacity><JSText>😮</JSText></TouchableOpacity>
-                </View>
-                <View style={styles.reactGroup}>
-                  <TouchableOpacity><JSText>😮</JSText></TouchableOpacity>
-                </View>
-                  </View>
-                  <View style={styles.reactColumn}>
-                  <View style={styles.reactGroup}>
-                    <TouchableOpacity><JSText>😮</JSText></TouchableOpacity>
-                  </View>
-                  <View style={styles.reactGroup}>
-                    <TouchableOpacity><JSText>😮</JSText></TouchableOpacity>
-                  </View>
-                    </View>
-                  <View style={styles.reactColumn}>
-                  <View style={styles.reactGroup}>
-                    <TouchableOpacity><JSText>😮</JSText></TouchableOpacity>
-                  </View>
-                  <View style={styles.reactGroup}>
-                    <TouchableOpacity><JSText>😮</JSText></TouchableOpacity>
-                  </View>
-                    </View>
-                </View>
+              
               </View>
           </ConfirmDialog>
           <CircleButton
